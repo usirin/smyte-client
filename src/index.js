e85699602db1071c2e499725e8e057e9dcff735b
Umut Sirin <umutsirin1@gmail.com>
2016-08-16 03:41:41 -0700

    Export module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,99 @@
-// @flow
+import invariant from 'invariant'
+import axios from 'axios'
 
-export default () => 'foo'
+const identity = x => x
+
+/**
+ * Initialize an object to interact with `smyte` api.
+ * @public
+ */
+export default class Smyte {
+  constructor(options = {}) {
+    invariant(
+      !!options.apiKey,
+      'smyte-client: expected apiKey to be present'
+    )
+
+    invariant(
+      !!options.apiSecret,
+      'smyte-client: expected apiSecret to be present'
+    )
+
+    this._apiKey = options.apiKey
+    this._apiSecret = options.apiSecret
+    this._classifyFn = options.transformClassifyResponse || identity
+    this._sendFn = options.transformSendResponse || identity
+
+    this.connection = axios.create({
+      baseURL: 'https://api.smyte.com',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      auth: {
+        username: this._apiKey,
+        password: this._apiSecret
+      }
+    })
+  }
+
+  /**
+   * Send an action to classify payload. Success handler will have
+   * classification result.
+   *
+   * @public
+   * @param {string} eventName - name of action
+   * @param {object} payload
+   * @returns {Promise}
+   */
+  classify(eventName, payload) {
+    return process(
+      this.connection, eventName, payload, '/v2/action/classify'
+    ).then(res => this._classifyFn(pickData(res)))
+  }
+
+  /**
+   * Send an action without waiting for an immediate result.
+   *
+   * @public
+   * @param {string} eventName
+   * @param {object} payload
+   * @returns {Promise}
+   */
+  send(eventName, payload) {
+    return process(
+      this.connection, eventName, payload, '/v2/action'
+    ).then(res => this._sendFn(pickData(res)))
+  }
+}
+
+/**
+ * Only return `data` from argument.
+ *
+ * @param {object}
+ * @returns {object}
+ */
+const pickData = res => res.data
+
+/**
+ * Process the event to smyte.
+ *
+ * @public
+ * @param {axios} connection
+ * @param {string} eventName
+ * @param {object} payload
+ * @param {string} url
+ * @returns {Promise}
+ */
+const process = (connection, eventName, payload, url) => {
+  payload = { ...payload, name: eventName }
+  return connection.post(url, payload)
+}
+
+/**
+ * Enum for classification types.
+ * @public
+ */
+Smyte.Classification = {
+  ALLOW: 'ALLOW',
+  BLOCK: 'BLOCK'
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,109 @@
+import path from 'path'
 import expect from 'expect'
+import vcr from 'nock-vcr-recorder'
 
-import mod from '../src'
+vcr.config({
+  cassetteLibraryDir: path.resolve('./test/cassettes')
+})
 
-describe('module', () => {
-  it('works', () => {
-    expect(mod()).toBe('foo')
+import Smyte from '../src'
+
+const API_KEY = 'key'
+const API_SECRET = 'secret'
+
+describe('smyte-client', () => {
+
+  it('should cry when there is no key and secret', () => {
+    expect(
+      () => new Smyte({})
+    ).toThrow(/smyte-client: expected apiKey to be present/)
+
+    expect(
+      () => new Smyte({apiKey: '123'})
+    ).toThrow(/smyte-client: expected apiSecret to be present/)
+  })
+
+  describe('#classify', () => {
+    it('should get a classification result', () => {
+      const smyte = new Smyte({ apiKey: API_KEY, apiSecret: API_SECRET })
+      const signupPayload = require('./action-fixtures/signup.json')
+
+      return vcr.useCassette('signup-classify', () => {
+        return smyte.classify('signup', signupPayload).then(classification => {
+          expect(classification.verdict).toBe(Smyte.Classification.ALLOW)
+        })
+      })
+    })
+
+    it('should allow for a custom transformer fn', () => {
+      const smyte = new Smyte({
+        apiKey: API_KEY,
+        apiSecret: API_SECRET,
+        transformClassifyResponse(raw) {
+          return new ClassifyResponse(raw)
+        },
+      })
+
+      const signupPayload = require('./action-fixtures/signup.json')
+      return vcr.useCassette('signup-classify', () => {
+        return smyte.classify('signup', signupPayload).then(response => {
+          expect(response.whoami).toBe('ClassifyResponse')
+        })
+      })
+    })
+  })
+
+  describe('#send', () => {
+    it('should send an action', () => {
+      const smyte = new Smyte({ apiKey: API_KEY, apiSecret: API_SECRET })
+      const signupPayload = require('./action-fixtures/signup.json')
+
+      return vcr.useCassette('signup-regular', () => {
+        return smyte.send('signup', signupPayload).then(response => {
+          expect(response.message).toBe(
+            'Action was logged.'
+          )
+        })
+      })
+    })
+
+    it('should allow for a custom transformer fn', () => {
+      const smyte = new Smyte({
+        apiKey: API_KEY,
+        apiSecret: API_SECRET,
+        transformSendResponse(raw) {
+          return new SendResponse(raw)
+        },
+      })
+
+      const signupPayload = require('./action-fixtures/signup.json')
+      return vcr.useCassette('signup-regular', () => {
+        return smyte.send('signup', signupPayload).then(response => {
+          expect(response.whoami).toBe('SendResponse')
+        })
+      })
+    })
   })
 })
+
+class CustomResponse {
+  constructor(options = {}) {
+    Object.keys(options).forEach(key => {
+      this[key] = options[key]
+    })
+  }
+}
+
+class ClassifyResponse extends CustomResponse {
+  constructor(options = {}) {
+    super(options)
+    this.whoami = 'ClassifyResponse'
+  }
+}
+
+class SendResponse extends CustomResponse {
+  constructor(options = {}) {
+    super(options)
+    this.whoami = 'SendResponse'
+  }
+}
